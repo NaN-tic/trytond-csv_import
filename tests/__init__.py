b7e7394502591805235f7e070a39db3236fba66b
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:16:11 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file is part of csv_import module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-from .test_csv_import import suite
-
-__all__ = ['suite']
